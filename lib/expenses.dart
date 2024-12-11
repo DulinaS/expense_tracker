4feceac4186449f0facfb3563080a6eef5ec6d8e
Dulina Senarathne <dulinahansa2002@gmail.com>
2024-12-11 23:04:15 +0530

    First commit

---
+++ lib/expenses.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+//Widget Class
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+//From build method we will return our widget tree
+class _ExpensesState extends State<Expenses> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        children: [
+          Text('The Chart'),
+          Text('Expenses list'),
+        ],
+      ),
+    );
+  }
+}
