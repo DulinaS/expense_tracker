6bebbfb68115cfe70a9741b82e768c6398c75071
Dulina Senarathne <dulinahansa2002@gmail.com>
2024-12-13 19:42:27 +0530

    Added Enums for expense Categories

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -4,15 +4,24 @@ import 'package:uuid/uuid.dart';
 
 const uuid = Uuid(); //Used to generate unique IDs- Utility Object
 
+enum Category { food , travel , leisure , work}
+
 class Expense {
   Expense({
     required this.title,   //We have used named parameters because using positional
+    
     required this.amount,  //Parameters, we need provide them in the same order that we have put method(int,int,String)
-    required this.date     // we have to provide data in same order int,int,String. So named parameters are easier
+    
+    required this.date,     // we have to provide data in same order int,int,String. So named parameters are easier
+    
+    required this.category, //There can be multiple expense types
+    
     }) : id = uuid.v4() ;  //Generates unique id and assign to 'id' whenever this 'Expense' class is initiated.    
 
   final String id;
   final String title;
   final double amount;
   final DateTime date; //built-in dart feature to store date
+  final Category category;
 }
+
