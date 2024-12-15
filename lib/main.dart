b89c71f949c99330751b664bc62f4024b09320e6
Dulina Senarathne <dulinahansa2002@gmail.com>
2024-12-15 19:58:16 +0530

    Added colors to more widgets

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,8 @@ import 'package:expense_tracker/widgets/expenses.dart';
 
 //Create a Color Scheme.  --> use k<variable_name> when declaring global varibales
 var kColorScheme = ColorScheme.fromSeed(
-  seedColor:const  Color.fromARGB(255, 96, 59, 181),
-  );
+  seedColor: const Color.fromARGB(255, 96, 59, 181),
+);
 
 void main() {
   runApp(
@@ -15,7 +15,26 @@ void main() {
         appBarTheme: const AppBarTheme().copyWith(
           backgroundColor: kColorScheme.onPrimaryContainer,
           foregroundColor: kColorScheme.primaryContainer,
-        )
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(  //titleLarge---->Changes all main title text style
+                fontWeight: FontWeight.normal,
+                color: kColorScheme.onSecondaryContainer,
+                fontSize: 14,
+              ),
+            ),
       ),
       home: const Expenses(),
     ),
