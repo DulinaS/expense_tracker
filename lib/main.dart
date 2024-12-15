d391097131a65cb389829a626ce82643b59b3314
Dulina Senarathne <dulinahansa2002@gmail.com>
2024-12-15 19:33:18 +0530

    Added a theme using custom color scheme

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,22 @@ import 'package:flutter/material.dart';
 
 import 'package:expense_tracker/widgets/expenses.dart';
 
+//Create a Color Scheme.  --> use k<variable_name> when declaring global varibales
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor:const  Color.fromARGB(255, 96, 59, 181),
+  );
+
 void main() {
   runApp(
-    const MaterialApp(
-      home: Expenses(),
+    MaterialApp(
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        )
+      ),
+      home: const Expenses(),
     ),
   );
 }
