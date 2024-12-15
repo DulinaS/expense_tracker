240e22b01d91aa3ff2bc371d8bdfe4fedd7b2ad9
Dulina Senarathne <dulinahansa2002@gmail.com>
2024-12-15 21:51:57 +0530

    Created additional Constructor in ExpenseBucket class. Added chart files

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -8,7 +8,7 @@ final formatter = DateFormat.yMd();
 
 const uuid = Uuid(); //Used to generate unique IDs- Utility Object
 
-enum Category { food , travel , leisure , work}
+enum Category { food, travel, leisure, work }
 
 //Icons for each category based on category types
 const categoryIcons = {
@@ -20,15 +20,15 @@ const categoryIcons = {
 
 class Expense {
   Expense({
-    required this.title,   //We have used named parameters because using positional
-    
-    required this.amount,  //Parameters, we need provide them in the same order that we have put method(int,int,String)
-    
-    required this.date,     // we have to provide data in same order int,int,String. So named parameters are easier
-    
+    required this.title, //We have used named parameters because using positional
+
+    required this.amount, //Parameters, we need provide them in the same order that we have put method(int,int,String)
+
+    required this.date, // we have to provide data in same order int,int,String. So named parameters are easier
+
     required this.category, //There can be multiple expense types
-    
-    }) : id = uuid.v4() ;  //Generates unique id and assign to 'id' whenever this 'Expense' class is initiated.    
+  }) : id = uuid
+            .v4(); //Generates unique id and assign to 'id' whenever this 'Expense' class is initiated.
 
   final String id;
   final String title;
@@ -37,32 +37,39 @@ class Expense {
   final Category category;
 
   //This is a getter to format the date---> For that we can import 3rd party package intl to format it
-  String getFormattedDate(){
+  String getFormattedDate() {
     return formatter.format(date);
   }
 }
 
 //Class to build the chart because we need sum of each category expenses
-class ExpenseBucket{
+class ExpenseBucket {
+  //Default constructor
   const ExpenseBucket({
     required this.category,
     required this.expenses,
   });
+
+  //Additional Constructor for the class to filter out the ones belong to a category
+  //Filters expenses belong to specific category
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+      : expenses = allExpenses
+            .where((expense) => expense.category == category)
+            .toList();
 
   final Category category;
   final List<Expense> expenses;
 
   //getter is same as a function but one that doesn't accept arguments
   //Returns total sum of the expenses
-  double get  totalExpenses {
+  double get totalExpenses {
     double sum = 0;
 
     //Goes every elements in expenses list
-    for(final expense in expenses){
+    for (final expense in expenses) {
       sum += expense.amount; //sum = sum + expense.amount
     }
 
     return sum;
   }
 }
-
